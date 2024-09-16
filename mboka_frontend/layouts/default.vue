e90b495b61dec9847b3105a361fea8e265826f41
VictorBK <victorbrian4@gmail.com>
2024-09-16 12:37:59 +0300

    Login logout authentication distinction

--- mboka_frontend/layouts/default.vue
+++ mboka_frontend/layouts/default.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { userUserStore } from "@/stores/user";
+
+const userStore = userUserStore()
+</script>
+
 <template>
     <div>
         <nav class="p-6 flex items-center justify-between bg-teal-800">
@@ -15,8 +21,16 @@
             <p class="text-gray-300">Copyright © 2024 -Mboka</p>
 
             <div class="flex mt-6 md:mt-0 items-center space-x-4">
-                <NuxtLink to="/login" class="py-4 px-6 bg-teal-900 hover:bg-teal-700 text-white rounded-xl">Log in</NuxtLink>
-                <NuxtLink to="/signup" class="py-4 px-6 bg-teal-600 hover:bg-teal-700 text-white rounded-xl">Sign up</NuxtLink>
+                <template v-if="userStore.user.isAuthenticated">
+                    <NuxtLink to="/myjobs" class="py-4 px-6 bg-teal-900 hover:bg-teal-700 text-white rounded-xl">My jobs</NuxtLink>
+                    <NuxtLink to="/createjobs" class="py-4 px-6 bg-teal-600 hover:bg-teal-700 text-white rounded-xl">Create job</NuxtLink>
+                    <NuxtLink to="/logout" class="py-4 px-6 bg-rose-600 hover:bg-rose-700 text-white rounded-xl">Log out</NuxtLink>
+                </template>
+
+                <template v-else>
+                    <NuxtLink to="/login" class="py-4 px-6 bg-teal-900 hover:bg-teal-700 text-white rounded-xl">Log in</NuxtLink>
+                    <NuxtLink to="/signup" class="py-4 px-6 bg-teal-600 hover:bg-teal-700 text-white rounded-xl">Sign up</NuxtLink>
+                </template>
             </div>
         </footer>
     </div>
