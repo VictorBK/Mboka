8dd89fa6326502274e89c406ea23b5966ce50008
VictorBK <victorbrian4@gmail.com>
2024-09-19 21:35:33 +0300

    dynamism

--- mboka_backend/job/views.py
+++ mboka_backend/job/views.py
@@ -3,12 +3,45 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Job, Category
-from .serializers import JobSerializer
+from .serializers import JobSerializer, JobDetailSerializer, CategorySerializer
+
+
+class CategoriesView(APIView):
+    def get(self, request, format=None):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+
+        return Response(serializer.data)
 
 
 class NewestJobsView(APIView):
     def get(self, request, format=None):
         jobs = Job.objects.all()[0:4]
         serializer = JobSerializer(jobs, many=True)
+
+        return Response(serializer.data)
+    
+
+class BrowseJobsView(APIView):
+    def get(self, request, format=None):
+        jobs = Job.objects.all()
+        categories = request.GET.get('categories', '')
+        query = request.GET.get('query', '')
+
+        if query:
+            jobs = jobs.filter(title__icontains=query)
+        
+        if categories:
+            jobs = jobs.filter(category_id__in=categories.split(','))
+            
+        serializer = JobSerializer(jobs, many=True)
+
+        return Response(serializer.data)
+    
+
+class JobsDetailView(APIView):
+    def get(self, request, pk, format=None):
+        job = Job.objects.get(pk=pk)
+        serializer = JobDetailSerializer(job)
 
         return Response(serializer.data)
