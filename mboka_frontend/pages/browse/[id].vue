b56cc8a8bbe7a687a958103b8f05fbfda98cdd3f
VictorBK <victorbrian4@gmail.com>
2024-09-19 21:34:14 +0300

    Browse is dynamic

--- mboka_frontend/pages/browse/[id].vue
+++ mboka_frontend/pages/browse/[id].vue
@@ -9,12 +9,16 @@ useSeoMeta({
     description: job.value.description
 })
 </script>
+
 <template>
     <div class="py-10 px-6 grid md:grid-cols-4 gap-3">
         <div class="md:col-span-3">
             <h1 class="mb-6 text-3xl">{{ job.title }}</h1>
 
-            <p>{{ job.description }}</p>
+            <p>
+                {{ job.description }}
+            </p>
+
             <a v-bind:href="'mailto:' + job.company_email" class="inline-block mt-6 py-4 px-6 bg-teal-700 text-white rounded-xl">Apply</a>
         </div>
 
@@ -22,18 +26,18 @@ useSeoMeta({
             <h3 class="mb-6 text-2xl">Company</h3>
 
             <p class="mb-2">{{ job.company_name }}</p>
-            <p>{{ job.company_location }}</p>
+            <p>Loca{{ job.company_locatoin }}tion</p>
 
             <hr class="my-6 opacity-30">
 
             <h3 class="mb-6 text-2xl">Position</h3>
+
             <p class="mb-2">{{ job.position_location }}</p>
             <p>{{ job.position_salary }}</p>
 
             <hr class="my-6 opacity-30">
 
             <p>Posted at {{ job.created_at_formatted }}</p>
-
         </div>
     </div>
 </template>
