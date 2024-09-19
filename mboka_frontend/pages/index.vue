006d12af561fa6f9654b90cea530046406f7ba72
VictorBK <victorbrian4@gmail.com>
2024-09-19 16:40:47 +0300

    Job retrieval

--- mboka_frontend/pages/index.vue
+++ mboka_frontend/pages/index.vue
@@ -1,3 +1,7 @@
+<script setup>
+const { data: jobs } = await useFetch('http://127.0.0.1:8000/api/v1/jobs/newest/')
+</script>
+
 <template>
     <div>
         <div class="py-20 px-6 bg-teal-700 text-center">
@@ -10,7 +14,7 @@
             <h2 class="mb-8 text-2xl text-center">Newest jobs</h2>
 
             <div class="space-y-4">
-                <job />
+                <job v-for="job in jobs" v-bind:key="job.id" v-bind:job="job" />        
                            
             </div>
         </div>
