e21ed379c34398ca78804447ef5e0ca7dfc105c8
VictorBK <victorbrian4@gmail.com>
2024-09-19 16:39:24 +0300

    Serialization

---
+++ mboka_backend/job/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from .models import Job, Category
+
+
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = (
+            'id',
+            'title',
+            'position_salary',
+            'position_location',
+            'company_name',
+            'created_at_formatted'
+        )
