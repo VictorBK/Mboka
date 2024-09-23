aeaecbda6c39aeffff2e275c1999ae9a7e829f7d
VictorBK <victorbrian4@gmail.com>
2024-09-23 05:05:39 +0300

    Made it possible to logout

--- mboka_frontend/layouts/default.vue
+++ mboka_frontend/layouts/default.vue
@@ -1,13 +1,18 @@
 <script setup>
-import { userUserStore } from "@/stores/user";
+import { useUserStore } from '@/stores/user'
 
-const userStore = userUserStore()
+const userStore = useUserStore()
+
+function logout() {
+    userStore.removeToken()
+}
 </script>
 
 <template>
     <div>
         <nav class="p-6 flex items-center justify-between bg-teal-800">
-            <NuxtLink to="/" class="text-xl text-white">Mboka</NuxtLink>
+            <NuxtLink to="/" class="text-xl text-white">Djob</NuxtLink>
+
             <div class="flex items-center space-x-4">
                 <NuxtLink to="/" class="text-white hover:text-teal-300">Home</NuxtLink>
                 <NuxtLink to="/browse" class="text-white hover:text-teal-300">Browse</NuxtLink>
@@ -17,13 +22,13 @@ const userStore = userUserStore()
         <slot />
 
         <footer class="p-6 flex flex-wrap items-center justify-between bg-gray-900">
-            <p class="text-gray-300">Copyright © 2024 -Mboka</p>
+            <p class="text-gray-300">Copyright (c) 2023 - Djob</p>
 
             <div class="flex mt-6 md:mt-0 items-center space-x-4">
                 <template v-if="userStore.user.isAuthenticated">
                     <NuxtLink to="/myjobs" class="py-4 px-6 bg-teal-900 hover:bg-teal-700 text-white rounded-xl">My jobs</NuxtLink>
                     <NuxtLink to="/createjob" class="py-4 px-6 bg-teal-600 hover:bg-teal-700 text-white rounded-xl">Create job</NuxtLink>
-                    <NuxtLink to="/" class="py-4 px-6 bg-rose-600 hover:bg-rose-700 text-white rounded-xl">Log out</NuxtLink>
+                    <a v-on:click="logout" class="py-4 px-6 bg-rose-600 hover:bg-rose-700 text-white rounded-xl">Log out</a>
                 </template>
 
                 <template v-else>
