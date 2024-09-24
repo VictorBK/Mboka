85cd906263d321002324653818c8250b383b1e1a
VictorBK <victorbrian4@gmail.com>
2024-09-24 14:24:37 +0300

    My jobs addition is now dynamic

--- mboka_frontend/pages/myjobs.vue
+++ mboka_frontend/pages/myjobs.vue
@@ -1,13 +1,46 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useUserStore } from '@/stores/user'
+
+const userStore = useUserStore()
+const router = useRouter()
+let jobs = ref()
+
+onMounted(() => {
+    if (!userStore.user.isAuthenticated) {
+        router.push('/login')
+    } else {
+        getJobs()
+    }
+})
+
+async function getJobs() {
+    await $fetch('http://127.0.0.1:8000/api/v1/jobs/my', {
+        headers: {
+            'Authorization': 'token ' + userStore.user.token,
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(response => {
+        jobs.value = response
+    })
+    .catch(error => {
+        console.log('error', error)
+    })
+}
+</script>
+
 <template>
     <div class="py-10 px-6">
         <h1 class="mb-6 text-2xl">My jobs</h1>
        
         <div class="space-y-4">
-            <job :my="true" />
-
-            <job :my="true" />
-
-            <job :my="true" />
+            <Job 
+                v-for="job in jobs"
+                :key="job.id"
+                :job="job" 
+                :my="true"             
+            />
         </div>    
     </div>
 </template>
