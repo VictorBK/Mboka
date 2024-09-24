9dbe5ca973b7da8ddd24f028952494d5a1a99388
VictorBK <victorbrian4@gmail.com>
2024-09-24 14:54:56 +0300

    Job deletion activated

--- mboka_frontend/pages/myjobs.vue
+++ mboka_frontend/pages/myjobs.vue
@@ -14,6 +14,12 @@ onMounted(() => {
     }
 })
 
+useSeoMeta({
+    title: 'My jobs',
+    ogTitle: 'My jobs',
+    description: 'The description'
+})
+
 async function getJobs() {
     await $fetch('http://127.0.0.1:8000/api/v1/jobs/my', {
         headers: {
@@ -28,19 +34,26 @@ async function getJobs() {
         console.log('error', error)
     })
 }
+
+function deleteJob(id) {
+    console.log('id', id)
+
+    jobs.value = jobs.value.filter(job => job.id !== id)
+}
 </script>
 
 <template>
     <div class="py-10 px-6">
         <h1 class="mb-6 text-2xl">My jobs</h1>
-       
+
         <div class="space-y-4">
-            <Job 
+            <Job
                 v-for="job in jobs"
                 :key="job.id"
                 :job="job" 
-                :my="true"             
+                :my="true"
+                v-on:deleteJob="deleteJob(job.id)"
             />
-        </div>    
+        </div>
     </div>
 </template>
